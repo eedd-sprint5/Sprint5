73b0abeca43d4f6ad6e890325ba28b9fac2bf629
Alvaro Lovera <loveraalvaro@gmail.com>
2018-04-10 13:01:46 +0200

    Extraccion superclase y encapsulacion campos

---
+++ src/Ejercicio01/CuentaBancaria.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio01;
+
+/**
+ *
+ * @author lover
+ */
+public class CuentaBancaria {
+    
+    /**
+     *
+     */
+    protected static final int min_nombre = 10;
+    /**
+     *
+     */
+    protected static final int max_nombre = 100;
+    protected String titular;
+    protected double saldo = 0;
+    protected String entidad;
+    protected String oficina;
+    protected String numCuenta;
+
+    public CuentaBancaria() {
+    }
+
+    
+    
+}
